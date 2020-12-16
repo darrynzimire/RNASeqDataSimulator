f6956526045dcdd9ce5751c5b7793ab58dc11dad
github.com/darrenzimire <darrenzim@gmail.com>
2020-12-15 14:03:17 -1100

    commit to repository

---
+++ Sessioninfo().py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# encoding = UTF-8
+
+from sinfo import sinfo
+
+import os
+import random
+import numpy as np
+import scipy as sp
+import argparse
+import pyfaidx
+import gzip
+import pickle as pickle
+from itertools import chain
+from SequenceContainer import ReadContainer
+from scipy.stats import uniform
+import os
+import sys
+import gzip
+import random
+import argparse
+from probability import DiscreteDistribution
+import random
+import copy
+import re
+import os
+import bisect
+
+import math
+import random
+import bisect
+import copy
+
+import re
+print('Other Python built-in libraries needed for RSDS:'
+      '{''')
+sinfo()
---
+++ docs/tools.md
@@ -0,0 +1,11 @@
+#Tools
+All scripts are written in python and can be found in the folder rsds/tools of the code repository
+Additionally, entry points for all scripts are available so they can be launched using just
+the script name. To use the scripts RSDS needs to be installed.
+
+### Generate a quality-score model: 
+learn_qmodel.py
+
+
+
+
