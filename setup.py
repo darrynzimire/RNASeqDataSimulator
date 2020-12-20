5085e2985bfd875df1e81f1e67bcb60a0a2d1c07
github.com/darrenzimire <darrenzim@gmail.com>
2020-12-20 11:17:39 -1100

    Created setup.py file

--- setup.py
+++ setup.py
@@ -1,16 +1,35 @@
-#!/usr/bin/python3
 # encoding = UTF-8
 
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-	name='RSDS',
-	description='Simulator for sequencing depth, read length and base-call quality in RNA-sequencing data',
-	long_description=open('README.md').read(),
-	version='0.2',
-	author='Darryn Zimire',
-	author_email='darrynzim@sun.ac.za',
-	license='MIT',
-	install_requires=['numpy', 'pyfaidx', 'scipy', 'pandas'],
+with open('README.md', 'r', encoding='utf-8') as fh:
+	long_description=fh.read()
 
-)
+setuptools.setup(
+	name="rsds",
+	version="0.0.1",
+	author="Darryn Zimire",
+	author_email="darrynzim@sun.ac.za",
+	description="Simulator for RNA-seq datasets for experimental design.",
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	license="MIT",
+	url='https://github.com/darrenzimire/RNASeqDataSimulator',
+	classifiers=[
+		"Intended Audience :: Science/Research",
+		"License :: MIT License",
+		"Natural Language :: English",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.5",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Topic :: Scientific/Engineering :: Bio-Informatics",
+		],
+	install_requires=["numpy", "matplotlib", "seaborn",
+					  "pyfaidx", "pandas", "biopython", "scipy",
+					  "scikit-learn"],
+	packages=setuptools.find_packages()
+
+	)
+
