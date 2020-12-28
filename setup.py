9dde6f71ab723cdee0ee0e8cf4be82c88d4035dd
Darryn <darrenzim@gmail.com>
2020-12-28 12:16:13 +0200

    updated setup.py and added tools to entry-points

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
 	name="rsds",
-	version="1.0.0",
+	version="0.1.0",
 	author="Darryn Zimire",
 	author_email="darrynzim@sun.ac.za",
 	description="Simulator for RNA-sequencing datasets to inform experimental design.",
