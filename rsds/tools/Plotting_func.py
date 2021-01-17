72179b5cf60ed2e7502d0fedee46b2e1602e92e6
darrynzimire <darrenzim@gmail.com>
2021-01-16 18:34:36 -1100

    Added plotting function to plot regression

--- rsds/tools/Plotting_func.py
+++ rsds/tools/Plotting_func.py
@@ -2,6 +2,10 @@
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 def plot_FLmodel_output(true_data, fitted_data):
@@ -27,8 +31,45 @@ def plot_aic_and_bic(n_components, aic, bic):
 	plt.show()
 
 
-def plot_regression():
-	pass
+def plot_regression(dfref, dfsim):
+	merged_set = pd.merge(dfref, dfsim, how='inner', on='transcript_id')
+	merged_set.head(5)
+	merged_set = merged_set.iloc[:, [0, 3, 4, 10, 11]]
+	merged_set.head(5)
+	x = merged_set.iloc[:, [1]].round()
+	y = merged_set.iloc[:, [4]].round()
+
+	# plt.scatter(x, y)
+	# data points
+	# Model initialization
+	regression_model = LinearRegression()
+	# # Fit the data(train the model)
+	regression_model.fit(x, y)
+	# # Predict
+	y_predicted = regression_model.predict(x)
+
+	# # model evaluation
+	rmse = mean_squared_error(y, y_predicted)
+	r2 = r2_score(y, y_predicted)
+
+	# # printing values
+	print('Slope:', regression_model.coef_)
+	print('Intercept:', regression_model.intercept_)
+	print('Root mean squared error: ', rmse)
+	print('R2 score: ', r2)
+
+	# plotting values
+	plt.scatter(x, y, s=10)
+	plt.xlabel('real data')
+	plt.ylabel('simulated data')
+
+	# # predicted values
+	plt.plot(x, y_predicted, color='r')
+	plt.savefig('real vs simulated expression profile.png')
+	plt.show()
+
+
+# plt.show()
 
 
 def plot_pca():
