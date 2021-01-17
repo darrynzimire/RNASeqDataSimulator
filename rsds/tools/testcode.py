297067ad40b6a9d981f8223f159743c4a2980fb1
darrynzimire <darrenzim@gmail.com>
2021-01-17 08:41:22 -1100

    init file

--- rsds/tools/testcode.py
+++ rsds/tools/testcode.py
@@ -3,13 +3,13 @@
 import pickle as pickle
 import gzip
 import pyfaidx
+import numpy as np
+from rsds.process_inputFiles import proc_tx_expmodel as ptx
 
-
-f = gzip.open('chr1FLmodel_p.gzip', 'rb')
-model = pickle.load(f)
-distparam= model[0]
-
-aic = model[1]
-bic = model[2]
-
-print(model)
+readtot = 500000
+f = 'test_subset_p.gz'
+model = ptx(f)
+id = model[2]
+# print(id)
+counts_p = np.rint(np.multiply(id, readtot)).astype(int)
+print(sum(counts_p))
