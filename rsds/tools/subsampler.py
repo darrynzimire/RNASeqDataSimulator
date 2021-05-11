d711152c1e57b77347bdbe780b6626897e73f6dd
Darryn <darrenzim@gmail.com>
2021-05-11 12:26:10 +0200

    updated writing of subsampled files

--- rsds/tools/subsampler.py
+++ rsds/tools/subsampler.py
@@ -1,20 +1,26 @@
 # encoding=utf-8
 
 import numpy as np
-import pandas as pd
-from Bio import SeqIO
 import random
 import argparse
 import gzip
-import io
+import sys
+
+# sampling from paired-end files
+# writing output to a folder
+# checks to make sure sampling is truely random
+# using filename function to write file names
+# writing out a log file
+# Perhaps an option for sampling reads through a theoretical distribution
 
 
 parser = argparse.ArgumentParser(description='FASTQ subsampler')
 parser.add_argument('--f1',         type=str, required=False, metavar='<str>', help='* input_read1.fq (.gz)"')
 parser.add_argument('--f2',         type=str, required=False, metavar='<str>', help='* input_read2.fq (.gz)"')
-parser.add_argument('--depth',      type=str, required=False, metavar='<str>', default=5000, help='')
+parser.add_argument('--depth',      type=int, required=False, metavar='<str>', default=5000, help='')
+parser.add_argument('--samples',    type=int, required=False, metavar='<str>', default=10, help='')
 parser.add_argument('--libtype',    type=str, required=False, metavar='<str>', default='paired', help='')
-parser.add_argument('--seed',       type=str, required=False, metavar='<str>', default=12345, help='')
+parser.add_argument('--seed',       type=int, required=False, metavar='<str>', default=12345, help='')
 parser.add_argument('--output',     type=str, required=False, metavar='<str>', default='rsdsv1.0.samples', help='')
 
 
@@ -22,6 +28,7 @@ args = parser.parse_args()
 f1 = args.f1
 f2 = args.f2
 depth = args.depth
+samples = args.samples
 libtype = args.libtype
 seed = args.seed
 output = args.output
@@ -52,23 +59,43 @@ def generatefilename(name, zipped, n, single_end):
 	return filenames
 
 
-def indexfile(file):
+print("counting records....")
+with gzip.open(f1) as input:
+	num_lines = sum([1 for line in input])
+total_records = int(num_lines / 4)
+#
+# if args.fraction:
+# 	args.number = int(total_records * args.fraction)
 
-	increment = 4
-	index = 0
-	totalreads = []
+print("sampling " + str(depth) + " out of " + str(total_records) + " records")
 
-	# if file.endswith('.gz'):
-	# 	f = gzip.open(file, 'rt')
-	# else:
-	f = open(file, 'r')
-	for line in f:
-		if (index % increment == 0):
-			totalreads.append(index/increment)
-		index +=1
-	print(totalreads)
+output_files = []
+output_sequence_sets = []
+for i in range(samples):
+	output_files.append(open(output + "." + str(i) + '.fastq', "w"))
+	output_sequence_sets.append(set(random.sample(range(total_records + 1), depth)))
 
-indexfile(f1)
+record_number = 0
+with gzip.open(f1) as input:
+		for line1 in input:
+			line2 = next(input).decode()
+			line3 = next(input).decode()
+			line4 = next(input).decode()
+			for i, output in enumerate(output_files):
+				if record_number in output_sequence_sets[i]:
+						output.write(line1.decode())
+						output.write(line2)
+						output.write(line3)
+						output.write(line4)
+				record_number += 1
+				if record_number % 100000 == 0:
+					print(str((int(record_number / total_records)) * 100) + " % done")
+
+
+for output in output_files:
+	output.close()
+print("done!")
+
 
 
 
